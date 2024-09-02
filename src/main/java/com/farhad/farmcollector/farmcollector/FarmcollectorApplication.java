9b59dd807ddcad87d92cf7b62527fc8ef7ab77dc
farhad kord <farhad.kurdi@gmail.com>
2024-09-02 12:11:42 +0300

    spring boot project is initiated

---
+++ src/main/java/com/farhad/farmcollector/farmcollector/FarmcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.farhad.farmcollector.farmcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmcollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farhad/farmcollector/farmcollector/FarmcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhad.farmcollector.farmcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
